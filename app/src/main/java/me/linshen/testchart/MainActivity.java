346865b68813107cace514db8047c0a23e7dee48
林申 <linshen@meizu.com>
2016-10-07 02:30:40 +0800

    change

--- app/src/main/java/me/linshen/testchart/MainActivity.java
+++ app/src/main/java/me/linshen/testchart/MainActivity.java
@@ -1,9 +1,13 @@
 package me.linshen.testchart;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import me.linshen.testchart.widget.PieChartView;
 
@@ -24,7 +28,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn) {
-
+            PieChartView.PieElement element1 = new PieChartView.PieElement("Twitter", 10, Color.YELLOW);
+            PieChartView.PieElement element2 = new PieChartView.PieElement("Facebook", 30, Color.BLACK);
+            PieChartView.PieElement element3 = new PieChartView.PieElement("Whatsapp", 20, Color.RED);
+            PieChartView.PieElement element4 = new PieChartView.PieElement("Meizu", 20, Color.BLUE);
+            PieChartView.PieElement element5 = new PieChartView.PieElement("Xiaomi", 20, Color.GRAY);
+            List<PieChartView.PieElement> list = new ArrayList<>();
+            list.add(element1);
+            list.add(element2);
+            list.add(element3);
+            list.add(element4);
+            list.add(element5);
+            mPieChartView.setPieElements(list);
         }
     }
 }
--- app/src/main/java/me/linshen/testchart/widget/PieChartView.java
+++ app/src/main/java/me/linshen/testchart/widget/PieChartView.java
@@ -1,37 +1,37 @@
 package me.linshen.testchart.widget;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import me.linshen.testchart.R;
 
 /**
- * @author vivek
+ * Created by linshen on 2016/10/6.
  */
-//========================================
 public class PieChartView extends View {
 
-    private final static String[] RColors = {"#448c9b", "#789a98", "#a4a795", "#ac6226", "#ef3c5e", "#b17170", "#9a9a82", "#bda763"};
+    private static final String TAG = "PieChartView";
 
     private RectF rec;
     private Paint p = new Paint();
-    private String[][] data = {
-            {"Facebook", "20"},
-            {"Twitter", "20"},
-            {"Whats app", "20"},
-            {"Pinterest", "20"},
-            {"Baby App", "20"}
-    };
+    private String[][] mData;
+    private int[] mColors = null;
+
     private int d = 0;
     private float mTextSize = 20f;
+    private int mStartAngle = 90;
+    private int mPieGap = 2;
 
     public PieChartView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
@@ -52,13 +52,14 @@ public class PieChartView extends View {
         }
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.PieChartView, 0, 0);
         try {
-            mTextSize = a.getDimension(R.styleable.PieChartView_android_text, 20f);
+            mTextSize = a.getDimension(R.styleable.PieChartView_android_textSize, 20f);
+            mStartAngle = a.getInt(R.styleable.PieChartView_startAngle, 90);
+            checkStartAngle();
         } finally {
             a.recycle();
         }
     }
 
-    //========================
     @Override
     public void onSizeChanged(int nw, int nh, int ow, int oh) {
         super.onSizeChanged(nw, nh, ow, oh);
@@ -69,36 +70,71 @@ public class PieChartView extends View {
 
     public void setTextSize(float size) {
         mTextSize = size;
+        invalidate();
     }
 
     /**
-     * Set data to be shown in pie chart view
+     * Set the default text size to a given unit and value.  See {@link
+     * TypedValue} for the possible dimension units.
      *
-     * @param dList ArrayList of Object[], 1st element will contain the name, 2nd the percentage (int)
+     * @param unit The desired dimension unit.
+     * @param size The desired size in the given units.
      */
-    public void setData(ArrayList<Object[]> dList) {
-        data = new String[dList.size()][2];
-        int i = 0;
-        for (Object[] op : dList) {
-            data[i++] = new String[]{op[0] + "", op[1] + ""};
+
+    public void setTextSize(int unit, float size) {
+        Context c = getContext();
+        Resources r;
+        if (c == null) {
+            r = Resources.getSystem();
+        } else {
+            r = c.getResources();
+        }
+        setTextSize(TypedValue.applyDimension(
+                unit, size, r.getDisplayMetrics()));
+    }
+
+    /**
+     * Set the default start angle to a given value.
+     *
+     * @param angle The desired start angle. Value <b>MUST</b> between 0 and 360.
+     */
+    public void setStartAngle(int angle) {
+        mStartAngle = angle;
+        checkStartAngle();
+        invalidate();
+    }
+
+    public void setPieElements(List<PieElement> elements) {
+        if (elements != null && elements.size() > 0) {
+            mData = new String[elements.size()][2];
+            mColors = new int[elements.size()];
+            int dataIndex = 0;
+            int colorIndex = 0;
+            for (PieElement element : elements) {
+                mData[dataIndex++] = new String[]{element.name + "", element.percentage + ""};
+                mColors[colorIndex++] = element.color;
+            }
+            postInvalidate();
         }
     }
 
-    //========================
     @Override
     public void onDraw(Canvas c) {
-
-        int size = data.length;
-        int startAngle = 0, endAngle = 0, ci = 0;
+        if (mData == null || mColors == null) {
+            return;
+        }
+        int size = mData.length;
+        int startAngle = mStartAngle, endAngle, colorIndex = 0;
         //== draw arc
         for (int i = 0; i < size; i++) {
-            int perc = Integer.parseInt(data[i][1]);
-            int pdeg = (perc * 360) / 100;
-            endAngle = startAngle + pdeg;
-            p.setColor(Color.parseColor(RColors[ci++]));
-            if (ci == RColors.length)
-                ci = 0;
-            c.drawArc(rec, startAngle + 2, pdeg - 2, true, p);
+            int percentage = Integer.parseInt(mData[i][1]);
+            int degree = p2d(percentage);
+            endAngle = startAngle + degree;
+            Log.d(TAG, "draw arc, startAngle: " + startAngle + ", end angle: " + endAngle);
+            p.setColor(mColors[colorIndex++]);
+            if (colorIndex == mColors.length)
+                colorIndex = 0;
+            c.drawArc(rec, startAngle + mPieGap, degree - mPieGap, true, p);
             startAngle = endAngle;
         }
 
@@ -110,25 +146,62 @@ public class PieChartView extends View {
         p.setColor(Color.BLACK);
         p.setFakeBoldText(true);
         p.setTextSize(mTextSize);
-        startAngle = 0;
-        endAngle = 0;
-        ci = 0;
+        startAngle = mStartAngle;
+        colorIndex = 0;
         double ra = 0; // Radian angle
         for (int i = 0; i < size; i++) {
-            p.setColor(Color.parseColor(RColors[ci++]));
-            if (ci == RColors.length)
-                ci = 0;
-            int perc = Integer.parseInt(data[i][1]);
-            int pdeg = (perc * 360) / 100;
-            endAngle = startAngle + pdeg; //== in degrees
-            ra = (startAngle + pdeg / 2) * Math.PI / 180;
+            p.setColor(mColors[colorIndex++]);
+            if (colorIndex == mColors.length)
+                colorIndex = 0;
+            int percentage = Integer.parseInt(mData[i][1]);
+            int degree = p2d(percentage);
+            endAngle = startAngle + degree; //== in degrees
+            ra = (startAngle + percentage / 2) * Math.PI / 180;
             int x = (int) (rec.right / 2 + (((rec.right / 2) * .5) * Math.cos(ra)));
             int y = (int) (rec.right / 2 + (((rec.right / 2) * .5) * Math.sin(ra)));
-            String text = data[i][0];
+            String text = mData[i][0];
             c.drawText(text, x - p.measureText(text) / 2, y, p);
-            text = data[i][1] + "%";
+            text = mData[i][1] + "%";
             c.drawText(text, x - p.measureText(text) / 2, y - p.ascent() + p.descent(), p);
             startAngle = endAngle;
+        }
+    }
+
+    private int p2d(int percentage) {
+        if (percentage < 0) {
+            percentage = 0;
+        } else if (percentage > 100) {
+            percentage = 100;
+        }
+        return (percentage * 360) / 100;
+    }
+
+    private void checkStartAngle() {
+        if (mStartAngle < 0) {
+            mStartAngle = 0;
+        } else if (mStartAngle > 360) {
+            mStartAngle = 360;
+        }
+    }
+
+    public static class PieElement {
+        private String name;
+        private int percentage;
+        private int color;
+
+        public PieElement(String name, int percentage, int color) {
+            this.name = name;
+            this.percentage = percentage;
+            this.color = color;
+        }
+
+        @Override
+        public String toString() {
+            return "PieElement{" +
+                    "name='" + name + '\'' +
+                    ", percentage=" + percentage +
+                    ", color=" + color +
+                    '}';
         }
     }
 }
